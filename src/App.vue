7016004f421e64463779ce4ab700b3860c4405fc
qosmioo <79136983+qosmioo@users.noreply.github.com>
2024-04-19 00:22:32 +0300

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="app">
+    <post-form 
+      @create="createPost"
+    />
+    <post-list 
+      :posts="posts"
+    />
+  </div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm.vue"
+import PostList from "@/components/PostList.vue"
+
+export default {
+  components: {
+    PostList, PostForm
+  },
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'JavaScript', body: 'Описание поста'},
+        {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+        {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
+        {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
+      ],
+      title: '',
+      body: ''
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post)
+    }
+  }
+}
+</script>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="post-list">
+    <h3>Список пользователей</h3>
+    <post-item
+      v-for="post in posts"
+      :post="post"
+    />
+  </div>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem.vue";
+export default {
+  components: {PostItem},
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.post-list{
+  margin: 15px;
+}
+</style>
